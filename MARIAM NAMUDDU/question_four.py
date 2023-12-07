dbd570497d308156cac98a3648f881e64715a04e
code-gi <143076573+code-gi@users.noreply.github.com>
2023-12-07 01:26:06 -0800

    Add files via upload

---
+++ MARIAM NAMUDDU/question_four.py
@@ -0,0 +1,20 @@
+#part one
+h=[1,12,123,1234,12345]
+for y in h:
+    print(y)
+
+#part 2
+import math
+print(math.factorial(5))
+
+
+#part 3
+a=3
+b=4
+c=(a+b)
+print(c)
+
+
+#part 4
+Car=input('brand and year')
+
---
+++ MARIAM NAMUDDU/question_three.py
@@ -0,0 +1,21 @@
+w=input("enter your age")
+
+
+
+
+students_marks=(input('enter your marks'))
+students_marks=(input('enter your marks'))
+students_marks=(input('enter your marks'))
+students_marks=(input('enter your marks'))
+students_marks=(input('enter your marks'))
+def marks(a,b,c):
+   if a>b and a>c:
+       print(f"A")
+   elif b>a and b>c:
+       print(f"B")
+   else:
+       print(f"C")
+marks=(90,80,70)
+
+
+
